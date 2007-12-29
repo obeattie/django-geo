8ce418bed1ac46297047aad3519ada312f4c085f
oliver@obeattie.com <oliver@obeattie.com@1e0b6216-6b41-0410-976d-2381a99fa12b>
2007-12-29 21:58:15 +0000

    Updated unit-tests to include *some* tests for the models; more to come.

--- tests.py
+++ tests.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-"""Unit testing for this module."""
+"""Unit testing for this module's fields."""
 
+from geopy import geocoders as geopy_geocoders, distance as geopy_distance
 from django.test import TestCase
 from django.db import models
+from django.conf import settings
 from fields import PickledObjectField
-
-class TestingModel(models.Model):
-	pickle_field = PickledObjectField()
-
-class TestCustomDataType(str):
-	pass
+from test_assets import *
+import models as geo_models
 
 class PickledObjectFieldTests(TestCase):
 	def setUp(self):
@@ -24,6 +22,7 @@ class PickledObjectFieldTests(TestCase):
 	
 	def testDataIntegriry(self):
 		"""Tests that data remains the same when saved to and fetched from the database."""
+		print 'wee'
 		for value in self.testing_data:
 			model_test = TestingModel(pickle_field=value)
 			model_test.save()
@@ -37,3 +36,26 @@ class PickledObjectFieldTests(TestCase):
 			model_test = TestingModel(pickle_field=value)
 			model_test.save()
 			self.assertEquals(value, TestingModel.objects.get(pickle_field__exact=value).pickle_field)
+
+class GeocodingTest(TestCase):
+	def setUp(self, query='London, UK'):
+		self.query = query
+		self.location_object = geo_models.Location.objects.create(query=self.query)
+	
+	def testModelFunctions(self):
+		"""Tests that the various functions of the Location model perform as expected."""
+		# Test the co-ordinate dictionary
+		self.assertEquals({
+			'latitude': self.location_object.latitude,
+			'longitude': self.location_object.longitude,
+		}, self.location_object.coords)
+		# Test the co-ordinate two-tuple
+		self.assertEquals((self.location_object.latitude, self.location_object.longitude), self.location_object.coords_tuple)
+		# Test the name convenience function - we haven't set a friendly name so this should be equal
+		# to the query
+		self.assertEquals(self.query, self.location_object.query)
+	
+	def testGeocoding(self):
+		print 'Hello World.'
+		returned_query, correct_coords = getattr(geopy_geocoders, settings.DEFAULT_GEOCODER)(settings.GEOCODING_KEYS[settings.DEFAULT_GEOCODER]).geocode(self.query)
+		self.assertEquals(correct_coords, self.location_object.coords_tuple)
